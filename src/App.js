95a53ff720908b6c9b95c12fc98df67a6b52dcb5
Aman <abhi9680847835@gmail.com>
2023-11-04 23:39:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// 1df0c9c011654522921ae912aa68ac87
+const App = () => {
+  const apiKey = process.env.REACT_APP_NEWS_API;
+  
+  const[progress, setProgress] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <LoadingBar
+          color='red'
+          progress={progress}
+
+        />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={5} country='in' category='General' />}> </Route>
+          <Route path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={5} country='in' category='Business' />}> </Route>
+          <Route path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={5} country='in' category='Entertainment' />}> </Route>
+          <Route path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={5} country='in' category='Health' />}> </Route>
+          <Route path="/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={5} country='in' category='General' />}> </Route>
+          <Route path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={5} country='in' category='Science' />}> </Route>
+          <Route path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={5} country='in' category='Sports' />}> </Route>
+          <Route path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={5} country='in' category='Technology' />}> </Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/News.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+const News = (props) => {
+    
+
+    const [articles, setArticles] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [page, setPage] = useState(1)
+    const [totalResults, setTotalResults] = useState(0)
+
+    // {props.apiKey}
+    const updateNews = async () => {
+        props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+        setLoading(true);
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        props.setProgress(30);
+        // console.log(parsedData);
+        setArticles(parsedData.articles);
+        setTotalResults(parsedData.totalResults);
+        setLoading(false);
+        props.setProgress(100);
+    }
+
+    //using while learning fetch api
+    //componentDidMount ki jagah useEffect
+    useEffect(() => {
+        document.title = `${props.category} - ReadNews`;
+        updateNews();
+    }, [])
+    
+    const fetchMoreData = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+        setPage(page+1);
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        // console.log(parsedData);
+        setArticles(articles.concat(parsedData.articles));
+        setTotalResults(parsedData.totalResults);
+       
+    };
+    return (
+        <div className='container my-3'>
+            <h3 className='text-center m-3'>NewsMonkey - Top Headlines</h3>
+            {loading && <Spinner />}
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                
+                hasMore={articles.length !== totalResults}
+                loader={<Spinner />}
+            //scrollableTarget="scrollableDiv"
+            >
+                <div className="container">
+
+                    <div className="row">
+                        {articles.map((element, index) => {
+                            return <div className="col-md-4" key={index}>
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                            </div>
+                        })}
+                    </div>
+                </div>
+            </InfiniteScroll>
+
+        </div>
+    )
+}
+
+News.defaultProps = {
+    country: 'in',
+    pageSize: 10,
+    category: 'general'
+}
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+}
+
+export default News
